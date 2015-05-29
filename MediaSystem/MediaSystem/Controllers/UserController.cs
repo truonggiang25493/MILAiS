4e964abdea57219ad514a8a9703b252bb80932ff
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-05-29 13:18:50 +0900

    First Commit
    Use Case
    Database
    Source Code

---
+++ MediaSystem/MediaSystem/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediaSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
